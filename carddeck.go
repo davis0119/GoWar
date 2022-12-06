430885686ee318b793c55f73f48539d624c36274
Davis Tran <davistran0119@gmail.com>
2022-12-05 23:02:46 -0500

    deck drawing and battle

--- carddeck.go
+++ carddeck.go
@@ -21,6 +21,23 @@ func (c *Card) String() string {
 	return ""
 }
 
+func (c1 *Card) BattleAgainst(c2 *Card) int {
+	if c1.Value.Val < c2.Value.Val {
+		return -1 // loses
+	} else if c1.Value.Val == c2.Value.Val {
+		return 0 // tie (WAR)
+	} else {
+		return 1 // win
+	}
+}
+
+// Draw from the top of the deck, if you want random order, call shuffle first
+func Draw(d *Deck) (Card, Deck) {
+	card := d.PlayingCards[0]
+	d.PlayingCards = d.PlayingCards[1:]
+	return card, *d
+}
+
 func (d *Deck) DeckInit() {
 	suits := []Suit{
 		{"Spades"},
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
 func main() {
+	// initialize deck variable
 
+	// deal out cards to both players
+
+	/*
+		game := war.NewGame()
+		game.InitPlayer(user)
+		game.InitBot(difficulty)
+		game.SplitCards()
+
+		for !game.Over() {
+			game.CommenceRound() // if card ranks are the same
+			 within CommenceRound(), we check for ties and grant the option to forfeit
+				for both players like below
+				'''
+				if isWar {
+					playerForfeit := grantForfeitOption()
+				}
+				'''
+			game.PrintBattleResults()
+		}
+		game.AnnounceWinner()
+	*/
 }
