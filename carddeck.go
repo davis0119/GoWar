063e7235b28b3e1cf93ac761131d1b61e90c3445
Davis Tran <davistran0119@gmail.com>
2022-12-05 22:53:19 -0500

    deck initalization

--- carddeck.go
+++ carddeck.go
@@ -1,15 +1,52 @@
 package deck
 
-type Face String
-type Suit String
-type Value int
+type Suit struct {
+	Name string
+}
+type Value struct {
+	Name string
+	Val  int
+}
 
 type Card struct {
-	Face  Face
 	Suit  Suit
 	Value Value
 }
 
 type Deck struct {
 	PlayingCards []Card
+}
+
+func (c *Card) String() string {
+	return ""
+}
+
+func (d *Deck) DeckInit() {
+	suits := []Suit{
+		{"Spades"},
+		{"Clubs"},
+		{"Diamonds"},
+		{"Hearts"},
+	}
+	values := []Value{
+		{"Two", 2},
+		{"Three", 3},
+		{"Four", 4},
+		{"Five", 5},
+		{"Six", 6},
+		{"Seven", 7},
+		{"Eight", 8},
+		{"Nine", 9},
+		{"Ten", 10},
+		{"Jack", 11},
+		{"Queen", 12},
+		{"King", 13},
+		{"Ace", 14},
+	}
+	d.PlayingCards = nil
+	for _, v := range values {
+		for _, s := range suits {
+			d.PlayingCards = append(d.PlayingCards, Card{Suit: s, Value: v})
+		}
+	}
 }
