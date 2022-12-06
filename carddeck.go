54fa1b9db95cf5fd2d15ba981ffc8f5061569cb5
Davis Tran <davistran0119@gmail.com>
2022-12-05 23:02:46 -0500

    deck drawing and battle

--- carddeck.go
+++ carddeck.go
@@ -21,6 +21,22 @@ func (c *Card) String() string {
 	return ""
 }
 
+func (c1 *Card) BattleAgainst(c2 *Card) int {
+	if c1.Value.Val < c2.Value.Val {
+		return -1 // loses
+	} else if c1.Value.Val == c2.Value.Val {
+		return 0 // tie (WAR)
+	} else {
+		return 1 // win
+	}
+}
+
+func Draw(d *Deck) (Card, Deck) {
+	card := d.PlayingCards[0]
+	d.PlayingCards = d.PlayingCards[1:]
+	return card, *d
+}
+
 func (d *Deck) DeckInit() {
 	suits := []Suit{
 		{"Spades"},
