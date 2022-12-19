3a2f86e97d34b3a57bf0030ea3df8be96343b0f1
Davis Tran <davistran0119@gmail.com>
2022-12-18 19:49:43 -0500

    documentation

--- war.go
+++ war.go
@@ -6,6 +6,11 @@ import (
 	"./deck"
 )
 
+// Input: string
+// Return: bool
+// Prints the given prompt and scans for an input. Continually checks
+// the first character of the input and returns true if the input is 'y' or
+// false if the input is 'n'.
 func promptPlayer(prompt string) bool {
 	var input string
 	for {
@@ -23,15 +28,21 @@ func promptPlayer(prompt string) bool {
 	}
 }
 
+// Input: deck pointer, deck pointer
+// Checks if either of the decks have 0 cards and returns true if either one does.
 func gameOver(player *deck.Deck, bot *deck.Deck) bool {
 	return len(player.PlayingCards) == 0 || len(bot.PlayingCards) == 0
 }
 
-func AnnounceStatusQuo(player *deck.Deck, bot *deck.Deck) {
+// Input: deck pointer, deck pointer
+// Prints out the current status of the game (how many cards each player has).
+func announceStatusQuo(player *deck.Deck, bot *deck.Deck) {
 	fmt.Println("You:", len(player.PlayingCards), "cards |", "Opponent:", len(bot.PlayingCards), "cards")
 }
 
-func AnnounceWinner(player *deck.Deck, bot *deck.Deck) {
+// Input: deck pointer, deck pointer
+// Announces a winner if there is one.
+func announceWinner(player *deck.Deck, bot *deck.Deck) {
 	if len(player.PlayingCards) == 0 {
 		fmt.Println("YOU HAVE LOST THE WAR!")
 	} else if len(bot.PlayingCards) == 0 {
@@ -55,14 +66,14 @@ func main() {
 			declareBattle := promptPlayer("Wage battle? (y/n) ")
 			if declareBattle {
 				deck.CommenceRound(player, bot)
-				AnnounceStatusQuo(player, bot)
+				announceStatusQuo(player, bot)
 			} else {
 				break
 			}
 		}
 		fmt.Println("The game is over.")
-		AnnounceStatusQuo(player, bot)
-		AnnounceWinner(player, bot)
+		announceStatusQuo(player, bot)
+		announceWinner(player, bot)
 		// Restart game option.
 		gameInSession = promptPlayer("Do you wish to re-wage War? ")
 	}
